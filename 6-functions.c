30cd497186746940947204c5030c143a9392117f
alex <alexbarrows@gmail.com>
2024-06-12 22:39:40 +0000

    added 6-functions.c

---
+++ 6-functions.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * this is a file to demonstrated the different types 
+ * of functions and how they work
+ */
+int main(void)
+{
+	return_type function_name(parameter_list)
+	{
+/**
+ * body of the function
+ */
+		/**
+		 * statement
+		 */
+}
+}
+/**
+ * function declaration
+ */
+int add(int a, int b);
+
+in main(void)
+{
+	/**
+	 * we call the function and pass 3 and 4 as parameters
+	 */
+	int result = add(3, 4);
+	printf( "%d", result); 
+	/**
+	 * this will print formated text to the display, with the number 7
+	 */
+	return (0);
+}
+/**
+ * this is the function defifnition
+ */
+int add(int a, int b)
+	/**
+	 * the function adds the two parameters and returns the result
+	 */
+
+	return a + b;
+	}
